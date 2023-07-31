e13a5feb77eb92971acc95712f23c6fcf6648ec6
KamiK4M1 <webvis5555@gmail.com>
2023-07-31 11:04:38 +0700

    fix error

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -4,7 +4,6 @@ export default function Members(){
     return (
         <main className="container mx-auto">
             <h1 className='flex flex-row justify-center text-4xl p-2'>สมาชิกในห้องเรียน ม.4/14</h1>
-            <Member/>
         </main>
     )
 }
